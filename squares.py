19da982a0810f3d1deda4914c65288ffc0cd2825
ozzie <ozzierivera95@gmail.com>
2024-12-12 15:09:30 -0800

    test

---
+++ squares.py
@@ -0,0 +1,5 @@
+# The following example builds the same list of square numbers you saw earlier but uses a list comprehension:
+squares = [value**2 for value in range(1,11)]
+print(squares)
+
+# When you’re writing three or four lines of code to generate lists and it begins to feel repetitive, consider writing your own list comprehensions.
