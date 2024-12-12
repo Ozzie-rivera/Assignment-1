54f39aa855be7ce5b5ab90a67f52fa1b08bf5747
ozzie <ozzierivera95@gmail.com>
2024-12-12 12:10:28 -0800

    orientation follow along

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# Write and run a simple program that prints Hello, World! .
+message = "Hello World!"
+print(message)
+
+# Create a list of yourfavorite hobbies and print each one using a for loop.
+hobbies = ['reading', 'jiujitsu', 'chess', 'magic the gathering']
+for hobby in hobbies:
+    print(hobby)
+    
+    
+
+# Modify the list by adding, removing, and sorting the items using list methods (e.g., append(), pop(), sort()).
+hobbies.append('eating')
+print(hobbies)
+
+hobbies.pop(2)
+print(hobbies)
+
+hobbies.sort()
+print(hobbies)
